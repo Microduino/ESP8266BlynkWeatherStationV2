296b469315f00f0a73b5290558a1de17a439af30
yanglibin <yanglibin@microduino.cc>
2017-07-31 15:31:50 +0800

    新库气象站、自适应AM2321或SHT2X传感器

---
+++ ESP8266BlynkWeatherStationV2/userDef.h
@@ -0,0 +1,15 @@
+#define LIGHTPIN A0
+#define ETOHPIN  A2
+
+#define SSID "ylb"
+#define PASS "ylb12345"
+
+char auth[] = "b6313a2688fe4b18ac66f2688cc7bbde";
+
+//#define SSID "type your ssid"
+//#define PASS "type your pass"
+//
+//char auth[] = "type your blynk token";
+
+
+
